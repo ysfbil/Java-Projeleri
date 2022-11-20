2ee8ebe50e7cb56b43a604ffcddaa29e28a6c7e7
ysfbil <33799483+ysfbil@users.noreply.github.com>
2022-11-21 00:41:33 +0300

    Chain Method

---
+++ NYPDersler/src/Sunu4/ChainMethod.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package Sunu4;
+
+class A{
+    A getA(){
+        System.out.println("A getirildi");
+        return this;
+    }
+    
+    A display(){
+        System.out.println("Hello from A");
+        return this;
+    }
+}
+
+
+
+
+/**
+ *
+ * @author ysfbil
+ */
+public class ChainMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       A a=new A();
+       a.getA().display().display().display().getA();
+    }
+}
